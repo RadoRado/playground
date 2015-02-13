f4a2086b2654f2754597057cb98d0118b16710e4
Radoslav RadoRado Georgiev <radorado@hackbulgaria.com>
2015-02-14 01:12:39 +0200

    Add generator for evens and odds

--- logic/generators.pl
+++ logic/generators.pl
@@ -2,3 +2,11 @@ int(0).
 int(X) :- int(Y), X is Y + 1.
 
 squares(X) :- int(Y), X is Y * Y.
+
+divisible(X, Y) :- 0 is X mod Y, !.
+
+even(X) :- divisible(X, 2).
+odd(X) :- not(even(X)).
+
+evens(X) :- int(X), even(X).
+odds(X) :- int(X), odd(X).
