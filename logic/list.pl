31b234024fad42ac9e77c74bbb39beb3bce2b1c3
Radoslav RadoRado Georgiev <radorado@hackbulgaria.com>
2015-02-14 01:13:02 +0200

    Add predicate for summing elements in list

--- logic/list.pl
+++ logic/list.pl
@@ -15,3 +15,8 @@ permutation([T | H], X) :- permutation(H, H1),
                          append(L1, L2, H1), 
                          append(L1, [T], X1), 
                          append(X1, L2, X).
+
+sum(L, R) :- sum(L, 0, R).
+
+sum([], F, F).
+sum([H | T], ACC, R) :- ACC2 is ACC + H, sum(T, ACC2, R).
