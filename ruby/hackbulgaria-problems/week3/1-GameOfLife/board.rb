4637b4f9cf334bbbc3ec6143803d1ee65467ed35
RadoRado <radorado@hackbulgaria.com>
2015-08-23 16:24:44 +0300

    Refactor the GoF board even more

--- ruby/hackbulgaria-problems/week3/1-GameOfLife/board.rb
+++ ruby/hackbulgaria-problems/week3/1-GameOfLife/board.rb
@@ -23,31 +23,34 @@ module GameOfLife
     
     def next_generation
       new_board = allocate_board
-      cells = @board.flatten
 
-      cells.each do |cell|
-        neighbours_status = neighbours(cell)
-        
-        alive_neighbours = neighbours_status["alive"]
-
-        if cell.is_alive
-          if alive_neighbours < 2  || alive_neighbours > 3 
-            new_board[cell.x][cell.y].die
-          end
-
-          if [2, 3].include? alive_neighbours 
-            new_board[cell.x][cell.y].bring_to_life
-          end 
-        end
-        if !cell.is_alive
-          if alive_neighbours == 3
-            new_board[cell.x][cell.y].bring_to_life
-          end
-        end
-
+      @board.flatten.each do |cell|
+        next_generation_for_cell(cell, @board, new_board)        
       end
 
       @board = new_board
+    end
+
+    def next_generation_for_cell(old_cell, old_board, new_board)
+      alive_count = alive_neighbours old_cell, old_board
+
+      if old_cell.is_alive
+        if alive_count < 2 || alive_count > 2 
+          new_board[old_cell.x][old_cell.y].die
+        end
+
+        if [2, 3].include? alive_count 
+          new_board[old_cell.x][old_cell.y].bring_to_life
+        end 
+      end
+
+      if !old_cell.is_alive
+        if alive_count == 3
+          new_board[old_cell.x][old_cell.y].bring_to_life
+        end
+      end
+
+      new_board
     end
 
     def to_s
@@ -74,20 +77,20 @@ module GameOfLife
       x >= 0 and x < @width and y >= 0 and y < @height
     end
 
-    def neighbours(cell)
-      Hash.new.tap do |h|
-        h["alive"] = 0
-        h["dead"] = 0
+    def alive_neighbours(cell, board)
+      alive_count = 0
 
-        GameOfLife::Board::MOVEMENTS.each do |movement|
-          dx, dy = movement
-          neighbour_x, neighbour_y = [cell.x + dx, cell.y + dy]
+      GameOfLife::Board::MOVEMENTS.each do |movement|
+        dx, dy = movement
+        neighbour_x, neighbour_y = [cell.x + dx, cell.y + dy]
 
-          if in_board(neighbour_x, neighbour_y) && @board[cell.x + dx][cell.y + dy].is_alive then h["alive"] += 1
-          else h["dead"] += 1
-          end
+        if in_board(neighbour_x, neighbour_y) && board[cell.x + dx][cell.y + dy].is_alive
+          alive_count += 1
         end
+
       end
+
+      alive_count
     end
   end
 end
